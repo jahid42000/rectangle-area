b1e2c73b94fb8c711dcdf1f3563ea52a63222b01
It's Jahid <106888651+jahid42000@users.noreply.github.com>
2022-06-14 10:40:19 +0600

    Add files via upload

---
+++ rectangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+
+    float length,wade,area;
+    printf(" enter length and area :");
+    scanf("%f%f",&length,&wade);
+    area = length*wade;
+    printf("ractngle area is :%.2f",area);
+
+
+}
